53a149c5616ca9c43b1829fa71457185f5a16002
unknown <bhanur05@gmail.com>
2019-12-19 14:23:58 +0530

    Initial commit

---
+++ src/test/java/com/bhanu/ibapsych/IbapsychApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhanu.ibapsych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbapsychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
